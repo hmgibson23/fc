69e89c4bd3c38dc3743de013d658451f95a17638
Hugo Gibson <hugo.gibson@yahoo.co.uk>
2015-07-25 12:26:00 +0100

    Generate the hash and the complete request uri

--- fyber-challenge/app/actions/offer_action.rb
+++ fyber-challenge/app/actions/offer_action.rb
@@ -1,13 +1,11 @@
 require "net/http"
 require "uri"
+require 'digest/sha1'
 
+
+# the constants
+OFFER_URI="http://api.sponsorpay.com/feed/v1/offers.json?"
 API_KEY="b07a12df7d52e6c118e5d47d3f9e60135b109a1f"
-
-# helper function to generate the url params
-def parameterize(params)
-  URI.escape(params.collect{|k,v| "#{k}=#{v}"}.join('&'))
-end
-
 FIXED_PARAMS = { :appid => 157,
                  :format => "json",
                  :device_id => "2b6f0cc904d137be2e1730235f5664094b83",
@@ -16,20 +14,31 @@ FIXED_PARAMS = { :appid => 157,
                  :offer_types => 112
                }
 
+# helper function to generate the url params
+def parameterize(params)
+  URI.escape(params.sort.collect{|k,v| "#{k}=#{v}"}.join('&'))
+end
 
-# use string interpolation to build an initial request uri
-OFFER_URI="http://api.sponsorpay.com/feed/v1/offers.json?"
+# helper to generate the hash
+def generateHash(params)
+  complete = parameterize(params) + '&' + API_KEY
+  Digest::SHA1.hexdigest complete
+end
 
 class OfferAction < Cramp::Action
   def start
     passed_params = { :uid => params[:uid],
                       :pub0 => params[:pub0],
-                      :page => params[:page]
+                      :page => params[:page],
+                      :timestamp => Time.now.to_i
                     }
 
+    complete_params = passed_params.merge(FIXED_PARAMS)
+    complete_params[:hashkey] = generateHash(complete_params)
 
-    res = { :received => params,
-            :req_uri => OFFER_URI + parameterize(FIXED_PARAMS) + parameterize(passed_params) }.to_json
+    request_uri = OFFER_URI + parameterize(complete_params)
+
+    res = { :req_uri =>  request_uri }.to_json
     render res
     finish
   end
