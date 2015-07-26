3286da2158a797e71c815a314c6151033a5c05c8
Hugo Gibson <hugo.gibson@yahoo.co.uk>
2015-07-26 08:07:39 +0100

    Split the global constants out into their own file

---
+++ fyber-challenge/app/globals.rb
@@ -0,0 +1,12 @@
+# the global constants
+OFFER_URI="http://api.sponsorpay.com/feed/v1/offers.json?"
+
+API_KEY="b07a12df7d52e6c118e5d47d3f9e60135b109a1f"
+
+FIXED_PARAMS = { :appid => 157,
+                 :format => "json",
+                 :device_id => "2b6f0cc904d137be2e1730235f5664094b83",
+                 :locale => "de",
+                 :ip => "109.235.143.113",
+                 :offer_types => 112
+               }
